785f68232d43fe98cb2c79f5b5e22b7d5a971738
Naoto Tachibana <n173337@edu.tut.ac.jp>
2020-11-02 16:42:40 +0900

    Update config.py

--- chainer_chemistry/config.py
+++ chainer_chemistry/config.py
@@ -4,4 +4,4 @@
 # --- Constant definitions ---
 # The maximum atomic number in rdkit
 MAX_ATOMIC_NUM = 117
-WEAVE_DEFAULT_NUM_MAX_ATOMS = 20  # 60  # paper
+WEAVE_DEFAULT_NUM_MAX_ATOMS = 0  # 60  # paper
